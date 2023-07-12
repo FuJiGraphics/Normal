3723dffaf9997ac9e5695b42d4e13a96e6a75738
FuJiGraphics <chlcjfwls1@naver.com>
2023-07-13 01:24:46 +0900

    Reset macro codes to previous.

--- Normal/src/Normal/Log.h
+++ Normal/src/Normal/Log.h
@@ -21,58 +21,59 @@ private:
 
 };
 
-#ifdef NR_BUILD_DLL // Normal Engine Core log macros
-    #define NR_TRACE(...)         Normal::Log::GetCoreLoger()->trace( __VA_ARGS__ );
-    #define NR_DEBUG(...)         Normal::Log::GetCoreLoger()->debug( __VA_ARGS__ );
-    #define NR_INFO(...)          Normal::Log::GetCoreLoger()->info( __VA_ARGS__ );
-    #define NR_WARN(...)          Normal::Log::GetCoreLoger()->warn( __VA_ARGS__ );
-    #define NR_ERROR(...)         Normal::Log::GetCoreLoger()->error( __VA_ARGS__ );
-    #define NR_CRITICAL(...)      Normal::Log::GetCoreLoger()->critical( __VA_ARGS__ );
 
-    #define NR_LINE_TRACE(...)    \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::trace, __VA_ARGS__);
+// Normal Engine Core log macros
+#define NR_CORE_TRACE(...)         Normal::Log::GetCoreLoger()->trace( __VA_ARGS__ );
+#define NR_CORE_DEBUG(...)         Normal::Log::GetCoreLoger()->debug( __VA_ARGS__ );
+#define NR_CORE_INFO(...)          Normal::Log::GetCoreLoger()->info( __VA_ARGS__ );
+#define NR_CORE_WARN(...)          Normal::Log::GetCoreLoger()->warn( __VA_ARGS__ );
+#define NR_CORE_ERROR(...)         Normal::Log::GetCoreLoger()->error( __VA_ARGS__ );
+#define NR_CORE_CRITICAL(...)      Normal::Log::GetCoreLoger()->critical( __VA_ARGS__ );
 
-    #define NR_LINE_DEBUG(...)    \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::debug, __VA_ARGS__);
+#define NR_CORE_LINE_TRACE(...)    \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::trace, __VA_ARGS__);
 
-    #define NR_LINE_INFO(...)     \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::info, __VA_ARGS__);
+#define NR_CORE_LINE_DEBUG(...)    \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::debug, __VA_ARGS__);
 
-    #define NR_LINE_WARN(...)     \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::warn, __VA_ARGS__);
+#define NR_CORE_LINE_INFO(...)     \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::info, __VA_ARGS__);
 
-    #define NR_LINE_ERROR(...)    \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::err, __VA_ARGS__);
+#define NR_CORE_LINE_WARN(...)     \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::warn, __VA_ARGS__);
 
-    #define NR_LINE_CRITICAL(...) \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::critical, __VA_ARGS__);
+#define NR_CORE_LINE_ERROR(...)    \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::err, __VA_ARGS__);
+
+#define NR_CORE_LINE_CRITICAL(...) \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetCoreLoger(), spdlog::level::critical, __VA_ARGS__);
 
 
-#else // Normal Engine Client log macros
-    #define NR_TRACE(...)       Normal::Log::GetClientLoger()->trace( __VA_ARGS__ );
-    #define NR_DEBUG( ... )     Normal::Log::GetClientLoger()->debug( __VA_ARGS__ );
-    #define NR_INFO(...)        Normal::Log::GetClientLoger()->info( __VA_ARGS__ );
-    #define NR_WARN(...)        Normal::Log::GetClientLoger()->warn( __VA_ARGS__ );
-    #define NR_ERROR(...)       Normal::Log::GetClientLoger()->error( __VA_ARGS__ );
-    #define NR_CRITICAL(...)    Normal::Log::GetClientLoger()->critical( __VA_ARGS__ );
+// Normal Engine Client log macros
+#define NR_CLIENT_TRACE(...)       Normal::Log::GetClientLoger()->trace( __VA_ARGS__ );
+#define NR_CLIENT_DEBUG( ... )     Normal::Log::GetClientLoger()->debug( __VA_ARGS__ );
+#define NR_CLIENT_INFO(...)        Normal::Log::GetClientLoger()->info( __VA_ARGS__ );
+#define NR_CLIENT_WARN(...)        Normal::Log::GetClientLoger()->warn( __VA_ARGS__ );
+#define NR_CLIENT_ERROR(...)       Normal::Log::GetClientLoger()->error( __VA_ARGS__ );
+#define NR_CLIENT_CRITICAL(...)    Normal::Log::GetClientLoger()->critical( __VA_ARGS__ );
 
-    #define NR_LINE_TRACE(...)    \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::trace, __VA_ARGS__);
+#define NR_CLIENT_LINE_TRACE(...)    \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::trace, __VA_ARGS__);
 
-    #define NR_LINE_DEBUG(...)    \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::debug, __VA_ARGS__);
+#define NR_CLIENT_LINE_DEBUG(...)    \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::debug, __VA_ARGS__);
 
-    #define NR_LINE_INFO(...)     \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::info, __VA_ARGS__);
+#define NR_CLIENT_LINE_INFO(...)     \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::info, __VA_ARGS__);
 
-    #define NR_LINE_WARN(...)     \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::warn, __VA_ARGS__);
+#define NR_CLIENT_LINE_WARN(...)     \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::warn, __VA_ARGS__);
 
-    #define NR_LINE_ERROR(...)    \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::err, __VA_ARGS__);
+#define NR_CLIENT_LINE_ERROR(...)    \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::err, __VA_ARGS__);
 
-    #define NR_LINE_CRITICAL(...) \
-        SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::critical, __VA_ARGS__);
+#define NR_CLIENT_LINE_CRITICAL(...) \
+    SPDLOG_LOGGER_CALL( Normal::Log::GetClientLoger(), spdlog::level::critical, __VA_ARGS__);
 
-#endif 
+
 } // namespace Normal Engine
