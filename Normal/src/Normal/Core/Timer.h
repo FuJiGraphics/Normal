4623fb7477f6d2c358e60521b560b3e6f94228a5
FuJiGraphics <chlcjfwls1@naver.com>
2023-08-28 22:25:12 +0900

    Created a Timer class.

--- Normal/src/Normal/Core/Timer.h
+++ Normal/src/Normal/Core/Timer.h
@@ -28,13 +28,13 @@ namespace Normal {
 		inline void Start() { m_PrevTime = m_CurrTime; m_Start = std::chrono::steady_clock::now(); }
 		inline void End()   { m_End = std::chrono::steady_clock::now(); m_CurrTime = m_End - m_Start; }
 
-		inline decltype( auto ) ElapsedTime( const T_Hours& type ) const { return NR_DURATION_CAST( m_CurrTime, hours ) }
-		inline decltype( auto ) ElapsedTime( const T_Minutes& type ) const { return NR_DURATION_CAST( m_CurrTime, minutes ) }
-		inline decltype( auto ) ElapsedTime( const T_Seconds& type ) const { return NR_DURATION_CAST( m_CurrTime, seconds ) }
+		inline decltype( auto ) ElapsedTime( const T_Hours& type )    const { return NR_DURATION_CAST( m_CurrTime, hours ) }
+		inline decltype( auto ) ElapsedTime( const T_Minutes& type )  const { return NR_DURATION_CAST( m_CurrTime, minutes ) }
+		inline decltype( auto ) ElapsedTime( const T_Seconds& type )  const { return NR_DURATION_CAST( m_CurrTime, seconds ) }
 		inline decltype( auto ) ElapsedTime( const T_Millisec& type ) const { return NR_DURATION_CAST( m_CurrTime, milliseconds ) }
 		inline decltype( auto ) ElapsedTime( const T_Microsec& type ) const { return NR_DURATION_CAST( m_CurrTime, microseconds ) }
-		inline decltype( auto ) ElapsedTime( const T_Nanosec& type ) const { return NR_DURATION_CAST( m_CurrTime, nanoseconds ) }
-
+		inline decltype( auto ) ElapsedTime( const T_Nanosec& type )  const { return NR_DURATION_CAST( m_CurrTime, nanoseconds ) }
+		
 	private:
 		StedyTimePoint m_Start;
 		StedyTimePoint m_End;
